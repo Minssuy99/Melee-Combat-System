4298ff8cef28283360bbe150114075c69fbc19ef
Minssuy <rlaalstjd6302@naver.com>
2025-05-16 15:14:32 +0900

    [2025-05-16] Update

--- Assets/Scripts/Combat System/MeleeFighter.cs
+++ Assets/Scripts/Combat System/MeleeFighter.cs
@@ -3,14 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttackState
+{
+    IDLE,
+    WINDUP,
+    IMPACT,
+    COOLDOWN,
+}
+
 public class MeleeFighter : MonoBehaviour
 {
+    [SerializeField] private GameObject sword;
+    
+    BoxCollider swordCollider;
+    
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
+
+    private void Start()
+    {
+        if (sword != null)
+        {
+            swordCollider = sword.GetComponent<BoxCollider>();
+            swordCollider.enabled = false;
+        }
+    }
+
+    AttackState attackState;
 
     public bool InAction { get; private set; } = false;
     
@@ -25,12 +48,48 @@ public class MeleeFighter : MonoBehaviour
     IEnumerator Attack()
     {
         InAction = true;
+        attackState = AttackState.WINDUP;
+
+        float impactStartTime = 0.33f;
+        float impactEndTime = 0.55f;
+        
         animator.CrossFade("Slash", 0.2f);
         yield return null;
 
         var animState = animator.GetNextAnimatorStateInfo(1);
-        
-        yield return new WaitForSeconds(animState.length);
+
+        float timer = 0f;
+
+        while (timer <= animState.length)
+        {
+            timer += Time.deltaTime;
+            float nomalizedTime = timer / animState.length;
+
+            if (attackState == AttackState.WINDUP)
+            {
+                if (nomalizedTime >=impactStartTime)
+                {
+                    attackState = AttackState.IMPACT;
+                    swordCollider.enabled = true;
+                }
+            }
+            else if (attackState == AttackState.IMPACT)
+            {
+                if (nomalizedTime >= impactEndTime)
+                {
+                    attackState = AttackState.COOLDOWN;
+                    swordCollider.enabled = false;
+                }
+            }
+            else if (attackState == AttackState.COOLDOWN)
+            {
+                // TODO : Handle combos
+            }
+            
+            yield return null;
+        }
+
+        attackState = AttackState.IDLE;
         
         InAction = false;
     }
